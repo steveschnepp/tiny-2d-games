dd44572a5fca7ed0ad6350492c25eaae9fd3a3d5
Steve Schnepp <steve.schnepp@pwkf.org>
2009-09-08 22:00:40 +0000

    - initial import of miniwars

---
+++ nds/miniwars/source/nds_utils.h
@@ -0,0 +1,16 @@
+// Copyright 2008-2009 Steve Schnepp <steve.schnepp@pwkf.org>
+#ifndef __NDS_UTILS_H__
+#define __NDS_UTILS_H__
+
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+bool PA_CheckLid();
+bool checkReset();
+
+#ifdef __cplusplus
+   }
+#endif
+
+#endif // __NDS_UTILS_H__
