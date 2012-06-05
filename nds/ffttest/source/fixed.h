a824390b56efc6354b6f1aaae6f67632809ef530
Steve Schnepp <steve.schnepp@pwkf.org>
2012-06-05 18:49:05 +0200

    - first version

---
+++ nds/ffttest/source/fixed.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef FIXED_H
+#define FIXED_H
+
+// Original code from https://gist.github.com/mtheall
+
+#include <nds.h>
+#include <math.h>
+
+class f32 {
+private:
+  s32 v;
+public:
+  f32(s32 v) : v(v) {}
+
+  f32(float f) : v(floattof32(f)) {}
+
+  s32 getValue() const { return v; }
+  v10 tov10() const { return (v>>3)&0x3FF; }
+  f32 const sqrt() const { return f32(sqrtf32(v)); }
+  f32 const sqr() const { return f32(mulf32(v, v)); }
+  f32 const abs() const { return (v >= 0) ? *this : f32(-v); }
+
+  int getInt() const { return f32toint(v); }
+
+  const f32 operator+(const f32 &f) const { return f32(v + f.v); }
+  const f32 operator-(const f32 &f) const { return f32(v - f.v); }
+  const f32 operator*(const f32 &f) const { return f32(mulf32(v, f.v)); }
+  const f32 operator/(const f32 &f) const { return f32(divf32(v, f.v)); }
+  const f32 operator-() const { return f32(-v); }
+
+  f32& operator+=(const f32 &f) { v += f.v; return *this; }
+  f32& operator-=(const f32 &f) { v -= f.v; return *this; }
+  f32& operator*=(const f32 &f) { v = mulf32(v, f.v); return *this; }
+  f32& operator/=(const f32 &f) { v = divf32(v, f.v); return *this; }
+
+  const bool operator< (const f32 &f) const { return v < f.v; }
+  const bool operator<=(const f32 &f) const { return v <= f.v; }
+  const bool operator> (const f32 &f) const { return v > f.v; }
+  const bool operator>=(const f32 &f) const { return v >= f.v; }
+  const bool operator==(const f32 &f) const { return v == f.v; }
+  const bool operator!=(const f32 &f) const { return v != f.v; }
+
+};
+
+// Helper functions that avoid OO-style
+f32 abs(f32 value) {
+        return value.abs();
+}
+
+f32 sqr(f32 value) {
+        return value.sqr();
+}
+
+
+#endif
---
+++ nds/ffttest/source/main.cpp
@@ -0,0 +1,161 @@
+#include <nds.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <list>
+
+#include "fixed.h"
+#include "fix_fft.h"
+
+u8  myBmp[192][256];
+
+template <class T> const T& normalize(const T& value, const T& min, const T& max) {
+	if (value < min) return min;
+	if (value > max) return max;
+	return value;
+}
+
+void drawPix(f32 fx, f32 fy, u8 color) {
+	int x = fx.getInt();
+	int y = fy.getInt();
+	if (x >= 0 && x < 256 && y >= 0 && y < 192) myBmp[y][x] = color;
+}
+
+//the record sample rate
+#define sample_rate  8000
+
+//buffer to hold sound data for playback
+u8* sound_buffer = 0;
+
+//buffer which is written to by the arm7
+u8* mic_buffer = 0;
+
+//the length of the current data
+u32 data_length = 0;
+
+// enough to hold a screen worth of 8bit audio
+u32 sound_buffer_size = 192*256;
+
+//the mic buffer sent to the arm7 is a double buffer
+//every time it is half full the arm7 signals us so we can read the
+//data.  I want the buffer to swap about once per frame so i chose a
+//buffer size large enough to hold two frames of 8bit mic data
+u32 mic_buffer_size = sample_rate / 30;
+
+//mic stream handler
+void micHandler(void* data, int length)
+{
+    if(!sound_buffer) return;
+
+    if(data_length > sound_buffer_size) data_length = 0;
+
+    DC_InvalidateRange(data, length);
+    dmaCopy(data, sound_buffer + data_length, length);
+
+    // DC_InvalidateAll ();
+    // memmove(sound_buffer + data_length, data, length);
+    data_length += length;
+}
+
+
+
+int main(int argc, char *argv[]) {
+	int down = 0;
+	static u32 frame = 0;
+
+	// We use the touch screen for graphical stuff
+	lcdMainOnBottom();
+
+	// set up video mode
+	videoSetMode(MODE_5_2D);
+	// set up VRAM banks
+	vramSetBankA(VRAM_A_MAIN_BG);
+	vramSetBankB(VRAM_B_MAIN_SPRITE);
+
+	// Set up console
+	videoSetModeSub(MODE_0_2D);
+	consoleDemoInit();
+
+	// initialize the backgrounds
+	int main = bgInit   (2, BgType_Bmp8, BgSize_B8_256x256, 0, 0);
+
+	// clear the backgrounds
+	memset(bgGetGfxPtr(main), 0, 256*256);
+
+	// fill in the palette
+	// gradient colors
+	for(u32 i = 0; i < 256; i++) { 
+		BG_PALETTE[i] = RGB15((i/1) % 32, (i/4) % 32, (i/8) % 32);
+		BG_PALETTE[i] = RGB15(i/8, i/8, i/8);
+	}
+
+	// Starting recording
+	sound_buffer = (u8*)malloc(sound_buffer_size);
+	mic_buffer = (u8*)malloc(mic_buffer_size);
+			
+	// Record
+	soundMicRecord(mic_buffer, mic_buffer_size, MicFormat_8Bit, sample_rate, micHandler);
+
+	// do stuff
+	do {
+		frame++;
+		cpuStartTiming(0);
+
+		if (down & KEY_R) {
+		}
+
+		if (down & KEY_L) {
+		}
+
+
+		if (down & KEY_DOWN) {
+		}
+		if (down & KEY_UP) {
+		}
+		if (down & KEY_LEFT) {
+		}
+		if (down & KEY_RIGHT) {
+		}
+
+		//for (u32 i = 0; i < data_length && i < 256*256; i++) {
+		u32 y = frame % 192;
+		u32 sound_offset = normalize<u32>(data_length - 256, 0, data_length);
+		
+		for (u32 x = 0; x < 256; x++) {
+			myBmp[y][x] = sound_buffer[sound_offset+x];
+
+			// Show current cursor
+			myBmp[(y+1) % 192][x] = 128;
+		} 
+
+		if (down & KEY_TOUCH) {
+			const int inverse = 0;
+			char* offset = (char*) myBmp[y];
+			fix_fftr(offset, 8, inverse);
+
+		}
+
+		int ticks_move = cpuGetTiming();
+
+		// wait for vblank
+		int cpu_usage = REG_VCOUNT;
+		swiWaitForVBlank();
+
+		int ticks_done = cpuEndTiming();
+		scanKeys();
+		down = keysCurrent();
+
+		// Print debug info
+		{ 
+			consoleClear(); 
+			printf("cpu: %.0f%%\n", cpu_usage / 1.92f);
+			printf("ticks: %d/%d (%.0f)\n", ticks_move, ticks_done, (100.0f * ticks_move / ticks_done));
+			printf("data_length: %d\n", data_length);
+		}	
+
+		// copy from buffer to vram
+		memcpy(bgGetGfxPtr(main), myBmp, sizeof(myBmp));
+	  } while(true);
+
+  return 0;
+}
